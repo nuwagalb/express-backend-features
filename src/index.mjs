21fcba982288cccc78b048acb39225d24df6bf43
nuwagalb <isnuwa@gmail.com>
2024-01-18 13:24:04 +0300

    add route parameters

--- src/index.mjs
+++ src/index.mjs
@@ -4,16 +4,31 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
+const mockUsers = [
+    {id: 1, name: "anson", displayName: "Anson"},
+    {id: 1, name: "jack", displayName: "Jack"},
+    {id: 1, name: "adam", displayName: "Adam"}
+]
+
 app.get("/", (request, response) => {
     response.status(201).send({msg: 'Hello!'})
 })
 
 app.get("/api/users", (request, response) => {
-    response.send([
-        {id: 1, name: "anson", displayName: "Anson"},
-        {id: 1, name: "jack", displayName: "Jack"},
-        {id: 1, name: "adam", displayName: "Adam"}
-    ])
+    response.send(mockUsers)
+})
+
+app.get("/api/users/:id", (request, response) => {
+    console.log(request.params)
+    const parsedId = parseInt(request.params.id)
+    if(isNaN(parsedId))
+        return response.status(400).send({msg: "Bad Request. Invalid ID."})
+
+    const findUser = mockUsers.find((user) => user.id === parsedId)
+
+    if (!findUser) return response.sendStatus(400)
+
+    return response.send(findUser)
 })
 
 app.get("/api/products", (request, response) => {
