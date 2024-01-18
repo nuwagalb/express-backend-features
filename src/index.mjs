55a63d4afefa57304539e41b26ef35921099dcb6
nuwagalb <isnuwa@gmail.com>
2024-01-18 14:13:18 +0300

    add query parameters

--- src/index.mjs
+++ src/index.mjs
@@ -5,17 +5,33 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 const mockUsers = [
-    {id: 1, name: "anson", displayName: "Anson"},
-    {id: 1, name: "jack", displayName: "Jack"},
-    {id: 1, name: "adam", displayName: "Adam"}
+    {id: 1, username: "anson", displayName: "Anson"},
+    {id: 2, username: "jack", displayName: "Jack"},
+    {id: 3, username: "adam", displayName: "Adam"}
 ]
+
+app.listen(PORT, () => {
+    console.log(`Running on Port ${PORT}`)
+})
 
 app.get("/", (request, response) => {
     response.status(201).send({msg: 'Hello!'})
 })
 
 app.get("/api/users", (request, response) => {
-    response.send(mockUsers)
+    console.log(request.query) //localhost/api/users?filter=username&value='an'
+    //destructure the query parameters
+    const {
+        query: {filter, value},
+    } = request
+
+    //if filter and value are defined
+    if (filter && value)
+        return response.send(
+            mockUsers.find((user) => user[filter].includes(value))
+        )
+
+    return response.send(mockUsers)
 })
 
 app.get("/api/users/:id", (request, response) => {
@@ -35,6 +51,3 @@ app.get("/api/products", (request, response) => {
     response.send([{id: 123, name: "Chicken Breast", price: 123.9}])
 })
 
-app.listen(PORT, () => {
-    console.log(`Running on Port ${PORT}`)
-})
