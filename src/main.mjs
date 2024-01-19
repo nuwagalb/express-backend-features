990249ce190ed1ac927331d3a885f23092275edc
nuwagalb <isnuwa@gmail.com>
2024-01-19 18:13:55 +0300

    add COOKIE implementation

--- src/main.mjs
+++ src/main.mjs
@@ -1,5 +1,6 @@
 import express from "express";
-import routes from "./routes/index.mjs"
+import routes from "./routes/index.mjs";
+import cookieParser, { signedCookie } from "cookie-parser";
 
 const app = express();
 
@@ -8,10 +9,14 @@ const PORT = process.env.PORT || 3000;
 //Global MiddleWare
 app.use(express.json());
 
+//cookie
+app.use(cookieParser("uvwxyz"));
+
 //users router
 app.use(routes)
 
 app.get("/", (request, response) => {
+        response.cookie("hello", "world", { maxAge: 60000 * 60 * 2, signed: true}) //1 minute = 60,000 milliseconds
         response.status(201).send({msg: 'Hello!'});
     }
 );
@@ -19,5 +24,10 @@ app.get("/", (request, response) => {
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`);
 });
+
+// Cookies: unique pieces of data that the server sends to the web browser
+// http is stateless that's why cookies are important so that they can
+// keep track of important data that the server and clients who use http
+// stateless protocol might want to maintain 
 
 
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,7 +3,15 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/products", (request, response) => {
-    response.send([{id: 123, name: "Chicken Breast", price: 123.9}]);
+    console.log(request.headers.cookie);
+    console.log(request.cookies);
+    console.log(request.signedCookies.hello);
+    if(request.signedCookies.hello && request.signedCookies.hello === "world")
+        return response.send([{id: 123, name: "Chicken Breast", price: 123.9}]);
+    
+    return response
+        .status(403)
+        .send({ msg: "Sorry. You need the correct cookie" }); 
 })
 
 export default router;
